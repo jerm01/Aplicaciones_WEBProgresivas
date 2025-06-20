84bda259e3b3a4591fb7d682e725e3908f143bcf
Enrique Alejandro Galván Montes <enrique.agm2177@gmail.com>
2025-06-19 23:56:16 -0600

    Fixed paths

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
       window.addEventListener('load', () => {
         navigator.serviceWorker
-          .register('service-worker.js')
+          .register('../serviceWorker.js')
           .then(reg => console.log('Service Worker registrado:', reg.scope))
           .catch(err => console.error('Error al registrar SW:', err));
       });
