dc37c6fc480f7a0ade7ecac0911e36f157e6e3e6
Enrique Alejandro Galván Montes <enriquegalvan@Enriques-MacBook-Air.local>
2025-06-19 21:18:23 -0600

    Added Service Worker

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const odsquiz = 'odsquiz-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  '/img/odsquiz-hero.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(odsquiz).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== odsquiz) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    }).catch(() => {
+    })
+  );
+});
